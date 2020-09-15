271a47228ab0282c4675a558fcba54337d63c58e
Jim Schmid <jim.schmid@1up.io>
2020-09-15 10:35:36 +0200

    Added ;

--- src/Resources/skeleton/content-element/tl_content.tpl.php
+++ src/Resources/skeleton/content-element/tl_content.tpl.php
@@ -9,6 +9,6 @@ $GLOBALS['TL_DCA']['tl_content']['palettes'] += [
     '<?= $element_name; ?>' => '
         {type_legend},type,headline;
         {expert_legend:hide},guests,cssID;
-        {invisible_legend:hide},invisible,start,stop
+        {invisible_legend:hide},invisible,start,stop;
     ',
 ];
